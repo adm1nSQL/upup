0c00df14ba145e08ccd80a6cb1abff858eb5f189
Koi <58238284+adm1nSQL@users.noreply.github.com>
2024-05-30 20:41:16 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>访问密码</title>
+</head>
+<body>
+    <h2>请输入访问密码</h2>
+    <form action="uuu.php" method="post">
+        密码: <input type="password" name="access_password">
+        <input type="submit" value="提交">
+    </form>
+</body>
+</html>
